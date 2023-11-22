51849694fcb27b8e59588f132d7b70daedb1aa76
Angga Dharma Syahputra <anggadharma1897@gmail.com>
2023-11-22 19:39:20 +0700

    "Tambah obat , spinner di hapus"

--- app/src/main/java/com/example/growzen/TambahObatActivity.kt
+++ app/src/main/java/com/example/growzen/TambahObatActivity.kt
@@ -10,22 +10,5 @@ class TambahObatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tambah_obat)
 
-        val spinner: Spinner = findViewById(R.id.spinner2)
-
-        // Contoh penggunaan ArrayAdapter untuk mengisi Spinner
-        val jenisObatArray = resources.getStringArray(R.array.Jenis_Obat)
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, jenisObatArray)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
-
-
-        // Menanggapi perubahan yang dipilih di Spinner
-//        spinner.setOnItemSelectedListener { _, _, position, _ ->
-//            val selectedJenisObat = jenisObatArray[position]
-//
-//        }
-
-        // Sisanya dari kode onCreate
-        // ...
     }
 }
