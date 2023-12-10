f6ef76ac7ae9cb2f18978327dd116595266c3e1b
Angga Dharma Syahputra <anggadharma1897@gmail.com>
2023-12-11 02:00:57 +0700

    "Edit Asset / Icon dan Menu Bar"

--- app/src/main/java/com/example/growzen/SharingFragment.kt
+++ app/src/main/java/com/example/growzen/SharingFragment.kt
@@ -55,9 +55,11 @@ class SharingFragment : Fragment() {
     private fun generateDummyData(): List<ModelSharing> {
         // You can replace this with your actual data retrieval logic
         val dummyList = mutableListOf<ModelSharing>()
-        dummyList.add(ModelSharing(R.drawable.pict_profile, "Nayeon", "12 October 2023", "SDH mw 3 bulan minum oat, 2 mnggu trakhir ni dr pinggul sampai lutut nyeri trutama saat mw bangun dr tidur, trus kalau di kaki ditekuk itu nyeri skalii, ada yg sama gak,??\n" +
+        dummyList.add(ModelSharing(
+            R.drawable.pp1, "Nayeon", "12 October 2023", "SDH mw 3 bulan minum oat, 2 mnggu trakhir ni dr pinggul sampai lutut nyeri trutama saat mw bangun dr tidur, trus kalau di kaki ditekuk itu nyeri skalii, ada yg sama gak,??\n" +
                 "Tnggl 13 ni kntrol dahak dan ronsen doakan yha smoga hasilnya membaik", 10, 9))
-        dummyList.add(ModelSharing(R.drawable.pict_profile, "Momo", "11 October 2023", "Halo. Saya mau tanya nih. Kemarin jadwal makan obat saya malam.\n" +
+        dummyList.add(ModelSharing(
+            R.drawable.pp3, "Momo", "11 October 2023", "Halo. Saya mau tanya nih. Kemarin jadwal makan obat saya malam.\n" +
                 "Tapi karena ketiduran jadi ingatnya pagi jam 5. Setelah itu sekarang keadaan saya persis seperti gejala awal. Mohon masukan bagi yg mengerti, apakah saya sudah kebal obat ya? "
             ,  20, 8))
         // Add more items as needed
