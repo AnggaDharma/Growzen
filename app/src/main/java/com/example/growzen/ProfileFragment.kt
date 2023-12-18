fa404dbf04d667f648bb7619db720c26da6de8f2
Angga Dharma Syahputra <anggadharma1897@gmail.com>
2023-12-18 19:23:01 +0700

    "menambhakan button save di data obat , menambhakan asset di activity profile , belum dilakukan intent

--- app/src/main/java/com/example/growzen/ProfileFragment.kt
+++ app/src/main/java/com/example/growzen/ProfileFragment.kt
@@ -1,10 +1,14 @@
 package com.example.growzen
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,9 +37,11 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+
 
     companion object {
         /**
